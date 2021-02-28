ce2eafeae3ffe11830a5447924f24609f8c66c8f
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-02-28 22:28:47 +0000

    Update MainActivity

--- app/src/main/java/com/fcalazans/drinksmenumanagement/MainActivity.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/MainActivity.java
@@ -1,14 +1,39 @@
 package com.fcalazans.drinksmenumanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fcalazans.drinksmenumanagement.databinding.ActivityMainBinding;
+import com.fcalazans.drinksmenumanagement.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ActivityMainBinding binding;
+    private MainViewModel mainViewModel;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initViewModel();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
+
+    // New way to use ViewModelProvider.
+    private void initViewModel() {
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    }
+
+
 }
