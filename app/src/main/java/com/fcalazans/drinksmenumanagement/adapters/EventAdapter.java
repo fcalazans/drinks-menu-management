ec43ff525608cd077cd72f9914b777283cff2023
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-03-03 06:46:59 +0000

    Create Adapter for Event

---
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/adapters/EventAdapter.java
@@ -0,0 +1,68 @@
+package com.fcalazans.drinksmenumanagement.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fcalazans.drinksmenumanagement.R;
+import com.fcalazans.drinksmenumanagement.database.entities.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+    private List<Event> events = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View iTemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_row,
+                parent, false);
+
+        return new EventViewHolder(iTemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+
+        Event currentEvent = events.get(position);
+        holder.textViewName.setText(currentEvent.eventTitle);
+        holder.textViewStartDate.setText(currentEvent.eventDateStart.toString());
+        holder.textViewEndDate.setText(currentEvent.eventDateEnd.toString());
+        holder.textViewDescription.setText(currentEvent.eventDescription);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+        notifyDataSetChanged();
+    }
+
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+        TextView textViewStartDate;
+        TextView textViewEndDate;
+        TextView textViewDescription;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.event_name);
+            textViewStartDate = itemView.findViewById(R.id.event_startDate);
+            textViewEndDate = itemView.findViewById(R.id.event_endDate);
+            textViewDescription = itemView.findViewById(R.id.event_description);
+
+        }
+    }
+}
