ff3907b9ac537059f67ca44f842d4a6b72122bac
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-03-03 06:49:16 +0000

    Format Event entity

--- app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Event.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Event.java
@@ -10,21 +10,27 @@ import java.util.Date;
 public class Event {
     @PrimaryKey(autoGenerate = true)
     public int event_id;
-
     @ColumnInfo(name = "event_title")
     public String eventTitle;
-
     @ColumnInfo(name = "event_description")
     public String eventDescription;
-
     @ColumnInfo(name = "event_type")
     public String eventType;
-
     @ColumnInfo(name = "date_start")
     public Date eventDateStart;
-
     @ColumnInfo(name = "date_end")
     public Date eventDateEnd;
 
+
+    public Event(String eventTitle, String eventDescription, String eventType, Date eventDateStart, Date eventDateEnd) {
+        this.eventTitle = eventTitle;
+        this.eventDescription = eventDescription;
+        this.eventType = eventType;
+        this.eventDateStart = eventDateStart;
+        this.eventDateEnd = eventDateEnd;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
 }
-// Add constructors?
