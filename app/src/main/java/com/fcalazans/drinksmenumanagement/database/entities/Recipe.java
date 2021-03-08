d21c74c35dedf32a2e39644db1935ea73261ea71
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-03-08 23:21:45 +0000

    Update Recipe entity.

--- app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Recipe.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Recipe.java
@@ -2,14 +2,29 @@ package com.fcalazans.drinksmenumanagement.database.entities;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "Recipe")
+@Entity(tableName = "Recipe",
+        foreignKeys = {@ForeignKey(entity = Glass.class,
+                parentColumns = "glass_id",
+                childColumns = "glass_idFk",
+                onDelete = ForeignKey.CASCADE)
+        }
+)
 public class Recipe {
     @PrimaryKey(autoGenerate = true)
     public int recipe_id;
 
     @ColumnInfo(name = "recipe_name")
     public String recipeName;
+
+
+    @ColumnInfo(name = "recipe_description")
+    public String recipeDescription;
+
+
+    @ColumnInfo(name = "glass_idFk")
+    public String recipeGlassware;
 
 }
