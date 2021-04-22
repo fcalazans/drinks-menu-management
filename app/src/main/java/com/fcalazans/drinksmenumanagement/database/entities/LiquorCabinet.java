0a12e56ee6f0daf90f150fc1ba709a14b5775bdd
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-04-22 16:18:41 +0100

    Create LiquorCabinet.java

---
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/LiquorCabinet.java
@@ -0,0 +1,22 @@
+package com.fcalazans.drinksmenumanagement.database.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "liquor_cabinet",
+        foreignKeys = {@ForeignKey(entity = Ingredient.class,
+                parentColumns = "ingredient_id",
+                childColumns = "ingredient_id",
+                onDelete = ForeignKey.CASCADE)
+        }
+)
+
+public class LiquorCabinet {
+
+    @PrimaryKey
+    @ColumnInfo(name = "ingredient_id")
+    public int ingredientId;
+
+}
