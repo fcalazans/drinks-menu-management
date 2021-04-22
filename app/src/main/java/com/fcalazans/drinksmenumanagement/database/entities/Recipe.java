b043e1fa0025bdc44e516465f2029f8de1126625
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-04-22 16:39:14 +0100

    Update Recipe.java

--- app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Recipe.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/Recipe.java
@@ -3,14 +3,17 @@ package com.fcalazans.drinksmenumanagement.database.entities;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "recipe",
         foreignKeys = {@ForeignKey(entity = Glass.class,
                 parentColumns = "glass_id",
                 childColumns = "glass_id",
-                onDelete = ForeignKey.CASCADE)
-        }
+                onDelete = ForeignKey.CASCADE,
+                onUpdate = ForeignKey.CASCADE)
+        },
+        indices = {@Index(value = {"glass_id"})}
 )
 public class Recipe {
     @PrimaryKey(autoGenerate = true)
