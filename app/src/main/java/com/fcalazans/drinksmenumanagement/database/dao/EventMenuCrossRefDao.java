4757a46ed66aa19395348896c3ecfbe77471dd3e
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-04-22 16:17:52 +0100

    Create EventMenuCrossRefDao.java

---
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/dao/EventMenuCrossRefDao.java
@@ -0,0 +1,29 @@
+package com.fcalazans.drinksmenumanagement.database.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import com.fcalazans.drinksmenumanagement.database.entities.EventMenuCrossRef;
+import com.fcalazans.drinksmenumanagement.database.relations.EventWithMenu;
+import com.fcalazans.drinksmenumanagement.database.relations.MenuWithEvent;
+
+import java.util.List;
+
+@Dao
+public interface EventMenuCrossRefDao {
+
+    @Query("SELECT * FROM eventmenucrossref")
+    LiveData<List<EventMenuCrossRef>> getAllEventMenuCrossRef();
+
+
+    @Transaction
+    @Query("SELECT * FROM Event")
+    LiveData<List<EventWithMenu>> getEventWithMenu();
+
+    @Transaction
+    @Query("SELECT * FROM Menu")
+    LiveData<List<MenuWithEvent>> getMenuWithEvent();
+
+}
