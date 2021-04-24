3f2b4b4d1ceb8d478339b2f74b75f2680f5a3137
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-04-24 15:01:15 +0100

    Home implementation

--- app/src/main/java/com/fcalazans/drinksmenumanagement/MainActivity.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/MainActivity.java
@@ -3,10 +3,7 @@ package com.fcalazans.drinksmenumanagement;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.fcalazans.drinksmenumanagement.adapters.EventAdapter;
 import com.fcalazans.drinksmenumanagement.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,18 +11,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_DrinksMenuManagement);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.event_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-
-        final EventAdapter adapter = new EventAdapter();
-        recyclerView.setAdapter(adapter);
-
-        eventViewModel = new MainViewModel(getApplication());
-        eventViewModel.getAllEvents().observe(this, adapter::setEvents);
+        getSupportActionBar().hide();
+//
+//        startActivity(new Intent(this, Activity.class));
+//        RecyclerView recyclerView = findViewById(R.id.event_list);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setHasFixedSize(true);
+//
+//        final EventAdapter adapter = new EventAdapter();
+//        recyclerView.setAdapter(adapter);
+//
+//        eventViewModel = new MainViewModel(getApplication());
+//        eventViewModel.getAllEvents().observe(this, adapter::setEvents);
     }
 }
