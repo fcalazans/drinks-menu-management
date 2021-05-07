1ab07bb3bde41ecda3659a20c5855225dfc4ccb3
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-05-07 17:29:07 +0100

    Update enforce ForeignKey constraint to CASCADE

--- app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/EventMenuCrossRef.java
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/database/entities/EventMenuCrossRef.java
@@ -5,15 +5,19 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.Index;
 
-@Entity(
+@Entity(tableName = "event_menu",
         primaryKeys = {"event_id", "menu_id"},
         foreignKeys = {
                 @ForeignKey(entity = Event.class,
                         parentColumns = "event_id",
-                        childColumns = "event_id"),
+                        childColumns = "event_id",
+                        onUpdate = ForeignKey.CASCADE,
+                        onDelete = ForeignKey.CASCADE),
                 @ForeignKey(entity = Menu.class,
                         parentColumns = "menu_id",
-                        childColumns = "menu_id")
+                        childColumns = "menu_id",
+                        onUpdate = ForeignKey.CASCADE,
+                        onDelete = ForeignKey.CASCADE)
         },
         indices = {@Index(value = {"menu_id"})}
 )
