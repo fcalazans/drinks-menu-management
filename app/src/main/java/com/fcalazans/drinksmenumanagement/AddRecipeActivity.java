fde81c81a640d90b82e1938ca07b879031d3aa66
Fabio Calazans <fcalazans@fabiocalazans.com>
2021-05-09 16:33:22 +0100

    Create functionality to add recipes

---
+++ app/src/main/java/com/fcalazans/drinksmenumanagement/AddRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.fcalazans.drinksmenumanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
